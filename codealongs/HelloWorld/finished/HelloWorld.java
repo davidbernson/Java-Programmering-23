a3f30ac5e0549dab71601a10fc76834199a0418f
David Bernson <david.j.bernson@gmail.com>
2023-08-25 15:47:31 +0200

    Refactoring of first lecture.

--- codealongs/HelloWorld/finished/HelloWorld.java
+++ codealongs/HelloWorld/finished/HelloWorld.java
@@ -1,5 +1,1 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
-    }
-}
+public class HelloWorld{public static void main(String[] args){System.out.print("Hello world!");}}
