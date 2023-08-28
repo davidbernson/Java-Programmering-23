4c60abf90d208ad38c46394b58785689238c4d5a
David Bernson <david.j.bernson@gmail.com>
2023-08-28 13:28:27 +0200

    Refactored Hello World back to normal.

--- codealongs/HelloWorld/finished/HelloWorld.java
+++ codealongs/HelloWorld/finished/HelloWorld.java
@@ -1,1 +1,5 @@
-public class HelloWorld{public static void main(String[] args){System.out.print("Hello world!");}}
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+    }
+}
