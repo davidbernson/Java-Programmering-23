234873a606295f6a17f73a818ab33dd6169cb364
Lukasalksater <lukalk0428@gmail.com>
2023-09-01 13:03:45 +0200

    Första övningen

---
+++ Excercise1.java
@@ -0,0 +1,17 @@
+public class Excercise1 {
+    public static int myAge = 19;
+    public static String myFirstName = "Lukas";
+    public static boolean doesItWork;
+
+    
+    public static void main(String[] args) {
+        
+        System.out.printf("I am " + myAge + " years old and my name is " + myFirstName);
+        doesItWork = true;
+
+
+
+    }
+    
+    
+}
