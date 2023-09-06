1f5f593ca33e0d16d0cb2a83a71cbe134decb7ed
davidbernson <david.j.bernson@gmail.com>
2023-09-06 10:29:29 +0200

    Changes to E1 solutions

--- codealongs/Print/finished/LearningPrint.java
+++ codealongs/Print/finished/LearningPrint.java
@@ -1,5 +1,18 @@
 package codealongs.Print.finished;
 
 public class LearningPrint {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(45);
+        System.out.println('\u00E4');
+
+        int cars = 8;
+
+        System.out.println(cars);
+
+        System.out.print("Hello World!");
+        System.out.print("Hello World!");
+
+        System.out.printf("Hello World!");
+    }
 }
