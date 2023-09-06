bb8104e0e513966d757e31785b41f810344bb59c
Lukasalksater <lukalk0428@gmail.com>
2023-09-06 10:18:35 +0200

    Changes

--- codealongs/Print/LearningPrint.java
+++ codealongs/Print/LearningPrint.java
@@ -3,6 +3,9 @@ package codealongs.Print;
 public class LearningPrint {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int car = 5;
+        System.out.println((car + " cars"));
     }
     
 }
