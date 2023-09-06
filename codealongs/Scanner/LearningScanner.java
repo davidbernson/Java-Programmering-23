f0d54b2f64db06a50be282eddb0a9dbfa720f011
Lukasalksater <lukalk0428@gmail.com>
2023-09-06 13:21:21 +0200

    Scanner test

--- codealongs/Scanner/LearningScanner.java
+++ codealongs/Scanner/LearningScanner.java
@@ -9,17 +9,14 @@ public class LearningScanner {
     int num = scanner.nextInt();
 
     if (num != 6 && num >0 && num <10){
-        System.out.println("Wrong");
+        System.out.println("Wrong, try again");
         scanner.close();
     }
     else if(num == 6){
       System.out.println("Write something");
       Scanner scanner2 = new Scanner(System.in);
       String newString = scanner2.nextLine();
-   
-      
-
-      
+            
       int wordLenght = newString.length();
       System.out.println(wordLenght + " characters written");
       scanner2.close();
@@ -30,14 +27,22 @@ public class LearningScanner {
     }
 
     else if (num  <0) {
-        System.out.println("No negative numbers");
+        System.out.println("No negative numbers, try again");
         scanner.close();
     }
 
     else if (num >10) {
-        System.out.println("Too high of a number");
+        System.out.println("Too high of a number, try again");
         scanner.close();
     }
+
+   else {
+    System.out.println("Wrong input");
+    scanner.close();
+   }
+
+    }
+
 
     }
 }
